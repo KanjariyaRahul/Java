6cf8faf9107e808b40ee264cc8bbf2d7b070a349
Rahul kanjariya <93303230+KanjariyaRahul@users.noreply.github.com>
2024-02-20 09:16:27 +0530

    Add files via upload

---
+++ java/Power.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Power {
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter Number");
+            int no = sc.nextInt();
+            int number=(int)Math.pow(no,2);
+            System.out.println("Number is : "+number);
+        }
+}
