44b62ce56222a87e97139e5d1e24853cd3412616
Rahul kanjariya <93303230+KanjariyaRahul@users.noreply.github.com>
2024-04-14 11:56:23 +0530

    Update Array_ele.java

--- “CIA-2 Practical Assignment/Array_ele.java
+++ “CIA-2 Practical Assignment/Array_ele.java
@@ -20,18 +20,22 @@ class Perform {
         int p = sc.nextInt();
 
         if (p >= 1 && p <= n + 1) {
-            for (int i = n; i >= p; i--) {
-                a[i] = a[i - 1];
-            }
-            a[p - 1] = e;
-            n++;
-            System.out.println("=============================");
-            for (int i = 0; i < n; i++) {
-                System.out.print(a[i] + " ");
-            }
+        int i = n;
+        while (i >= p) {
+            a[i] = a[i - 1];
+            i--;
+        }
+        a[p - 1] = e;
+        n++;
+    
+        System.out.println("=============================");
+        for (i = 0; i < n; i++) {
+            System.out.print(a[i] + " ");
+        }
         } else {
             System.out.println("Position Invalid");
         }
+
         sc.close();
     }
 }
